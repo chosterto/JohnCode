abfd4f4ffc1efb38d8f4ba36420d7b19221a349b
Michael Drouillard <mddrouillard@gmail.com>
2022-02-18 17:29:11 -0500

    a

--- src/main/cpp/subsystems/TurretSubsystem.cpp
+++ src/main/cpp/subsystems/TurretSubsystem.cpp
@@ -79,7 +79,6 @@ double TurretSubsystem::EstimateTurretAngle(double ticks) {
 }
 
 double TurretSubsystem::FindTurretError(double centerAngle, double turretAngle) {
-    // 
     if (centerAngle >= 0 && centerAngle < 90) {
         return (90 - turretAngle) + centerAngle;
     } 
